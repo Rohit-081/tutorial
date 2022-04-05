5d4e80f6f11250efc604068e929fd972b498ad60
Rohit Jain <Rohit.Jain@BRILLIO.COM>
2022-04-05 13:15:37 +0530

    react counter

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import './App.css';
 import Greet from '../src/components/Greet'
 import Welcome from '../src/components/Welcome'
 import Message from '../src/components/Message'
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
-     <Greet name="Bruce" heroname="batman"><p>
+    {/*  <Greet name="Bruce" heroname="batman"><p>
        This is a child props.</p></Greet>
      <Greet name="Clark" heroname="comman man"/>
      <Greet name="Diana" heroname="wonder woman" />
      <Welcome name="Bruce" heroname="batman" />
-     <Message />
+     <Message /> */}
+     <Counter />
     </div>
   );
 }
